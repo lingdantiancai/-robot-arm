a973193c7b8763b20048cea87a2f9143d2e51838
Attention355 <823727097@qq.com>
2016-10-30 15:21:56 +0800

    程序
    arduino检查程序有问题  应该是没有设置b变量，我直接输出pos

--- Robot-arm/Robot-arm.ino
+++ Robot-arm/Robot-arm.ino
@@ -16,7 +16,7 @@ void loop() {
   Serial.println(a);
   
   pos = map(a,0,1023,0,180);//这里将电位器所读到的值映射到0-180，舵机可移动范围
-  Serial.println(b);//这里输出的值便是角度。
+  Serial.println(pos);//这里输出的值便是角度。
   myservo.write(pos); 
   delay(30);
  
