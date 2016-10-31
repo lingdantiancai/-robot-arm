da7ae89936d9999ef0ee22811de7c5837894ecf3
TED <lingdantiancai@163.com>
2016-10-31 22:48:52 +0800

    单个舵机动作组的存储以及读取运行
    我用数组写了一个动作租的存储以及读取的程序，程序在IDE上编译通过，但是我没有实际进行测试。你们帮忙测试一下，看下可以正常运行不？

--- Robot-arm/Robot-arm.ino
+++ Robot-arm/Robot-arm.ino
@@ -1,17 +1,25 @@
 #include <Servo.h>
 Servo myservo;
+const int b =10;//动作组存储上限
+int myInts[b];//存储动作组的数组
+int i=0;//动作组的个数
+
 
 int pos = 0; 
 const int Dianweiqi1 = A0;//定义电位器接A引脚
 
 void setup() {
+  
   Serial.begin(9600);
+  pinMode(6,INPUT);//按键执行动作组
+  pinMode(5,INPUT);//按键存储角度
   pinMode(Dianweiqi1,INPUT);//定义电位器输入
   myservo.attach(9);
-
+  
 }
 
 void loop() {
+  
   int a = analogRead(Dianweiqi1);
   Serial.println(a);
   
@@ -19,5 +27,19 @@ void loop() {
   Serial.println(pos);//这里输出的值便是角度。
   myservo.write(pos); 
   delay(30);
- 
+  if(digitalRead(5) == HIGH)
+  {
+    myInts[i] = pos;
+    i++;
+    delay(500);
+    }
+      if(digitalRead(6) == HIGH)//按键后这里执行数组里面存储的动作组
+  {   
+    for(i=0; i<b; i++)
+    {
+      pos = myInts[i];
+      myservo.write(pos);
+      delay(100);
+    }
+    }
 }
